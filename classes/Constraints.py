52e3176e2676e67696105739414d9e40078763b3
Hamta Sedghani <hamtasedghani@Hamtas-MacBook-Pro.local>
2021-09-15 11:27:20 +0200

    updated

--- classes/Constraints.py
+++ classes/Constraints.py
@@ -1,96 +1,90 @@
+from classes.Performance import SystemPerformanceEvaluator
 from abc import ABC, abstractmethod
 import numpy as np
-import pdb
-import sys
+
+
 ## PerformanceConstraint
 #
 # Class used to represent the performance constraint a configuration is 
 # subject to
 class PerformanceConstraint(ABC):
 
-    ## @var threshold
+    ## @var max_res_time
+    # Upper-bound threshold for the response time
 
-    ## @var slack_value
-    
     ## PerformanceConstraint class constructor
     #   @param self The object pointer
-    #   @param max_res_time
-    #   @param slack_value
+    #   @param max_res_time Upper-bound threshold for the response time
     def __init__(self, max_res_time):
         self.max_res_time = max_res_time
-        
-
+    
     ## Method to check the feasibility of the constraint (abstract)
     #   @param self The object pointer
+    #   @param S A System.System object
+    #   @param solution A Solution.Configuration representing a candidate 
+    #                   solution of the problem
+    #   @return A boolean which is true if the constraint is satisfied and 
+    #           the corresponding response time
     @abstractmethod
-    def check_feasibility(self):
+    def check_feasibility(self, S, solution):
         pass
-    
-    ## Method to evaluate performance of component_idx
-    #   @param self The object pointer
-    #   @param S an instance of System class
-    #   @param y_hat a solution
-    #   @param component_idx The index of current component
-    def get_perf_evaluation(self,S,y_hat,component_idx):
-        
-        
-        # get the resource of current component and evaluat the performance of the component
-        j=np.nonzero(y_hat[component_idx])[0][0]
-       
-        if j<S.FaaS_start_index:
-            
-             perf_evaluation = S.resources[j].performance_evaluator.evaluate_component(
-                                     component_idx,j,y_hat, S)
-        else:
-           
-             perf_evaluation = S.resources[j].performance_evaluator.evaluate_component(
-                                     component_idx,j, S)
-        
-        return perf_evaluation
+
+
 
 ## LocalConstraint
 #
 # Specialization of PerformanceConstraint, related to a single Component
 class LocalConstraint(PerformanceConstraint):
     
-   
-    ## @var component
-    # Component the constraint is related to 
+    ## @var component_idx
+    # Index of the Graph.Component object the constraint is related to 
 
     ## LocalConstraint class constructor
     #   @param self The object pointer
-    #   @param component
-    #   @param max_res_time
-    #   @param slack_value
-   
-    
-    def __init__(self,  component_idx,max_res_time):
+    #   @param component_idx Index of the Graph.Component object the 
+    #                        constraint is related to 
+    #   @param max_res_time Upper-bound threshold for the response time
+    def __init__(self, component_idx, max_res_time):
         super().__init__(max_res_time)
-        self.component_idx=component_idx
+        self.component_idx = component_idx
         
-    
-    
-    
     
     ## Method to check the feasibility of the constraints
     #   @param self The object pointer
-    #   @param S an instance of system class
-    #   @param solution a solution of the problem
-    def check_feasibility(self,S,solution):
-            flag=False
-            # evaluate the performance of component
-            perf_evaluation=self.get_perf_evaluation(S,solution.Y_hat,self.component_idx)
+    #   @param S A System.System object
+    #   @param solution A Solution.Configuration representing a candidate 
+    #                   solution of the problem
+    #   @return A boolean which is true if the constraint is satisfied and 
+    #           the response time of the corresponding Graph.Component
+    def check_feasibility(self, S, solution):
+        
+        feasible = False
            
-            # check if the denumerator is equal to zero
-            if not np.isnan(perf_evaluation):
-                solution.local_slack_value[self.component_idx]= self.max_res_time - perf_evaluation
-                if perf_evaluation<=self.max_res_time:
-                    flag=True
-            else:
-                 solution.local_slack_value[self.component_idx]=float('Inf')  
-                 
-                 
-            return flag,perf_evaluation   
+        # evaluate the performance of component
+        PE = SystemPerformanceEvaluator()
+        perf_evaluation = PE.get_perf_evaluation(S, solution.Y_hat, 
+                                                 self.component_idx)
+        # check if the denumerator is equal to zero
+        if not np.isnan(perf_evaluation):
+            # update the slack value
+            solution.local_slack_value[self.component_idx] = self.max_res_time - perf_evaluation
+            # check feasibility
+            if perf_evaluation <= self.max_res_time:
+                feasible = True
+        else:
+            solution.local_slack_value[self.component_idx] = float('Inf')  
+        
+        return feasible, perf_evaluation
+    
+    
+    ## Operator to convert a LocalConstraint object into a string
+    #   @param self The object pointer
+    #   @param all_components List of all the Graph.Component objects in the 
+    #                         system
+    def __str__(self, all_components):
+        s = '"{}": {{"local_res_time":{}}}'.\
+            format(all_components[self.component_idx].name, self.max_res_time)
+        return s
             
 
 
@@ -103,121 +97,101 @@ class GlobalConstraint(PerformanceConstraint):
     ## @var path
     # List of Component objects the constraint is related to 
     
+    ## @var path_name
+    # Name of the path the constraint is related to
+    
     ## GlobalConstraint class constructor
     #   @param self The object pointer
-    #   @param path a list of component index which are in the Path
-    #   @param max_res_time
-    def __init__(self, path,max_res_time):
+    #   @param path List of indices of Graph.Component objects which are in 
+    #               a Path
+    #   @param max_res_time Upper-bound threshold for the response time
+    #   @param path_name Name of the current path
+    def __init__(self, path, max_res_time, path_name = ""):
         super().__init__(max_res_time)
         self.path = path
-        
-    ## Method to return the common network domain between the resource of first component and the second one
-    #   @param self The object pointer
-    #   @param cpm1_resource resource of first component
-    #   @param cpm1_resource resource of second component
-    #   @param S instance of System class
-    #   @param return common network domain
-    def get_network_delay(self,comp_index,cpm1_resource,cpm2_resource,S):
-        comp1_key = [key for key, value in S.dic_map_com_idx.items() if value == comp_index][0]
-      
-        # CL1=list(filter(lambda resource: (cpm1_resource in resource), S.resources_CL))[0][1]
-        # CL2=list(filter(lambda resource: (cpm2_resource in resource), S.resources_CL))[0][1]
-        CL1=next((cl.name for cl in S.CLs if cpm1_resource in cl.resources ), None)
-        CL2=next((cl.name for cl in S.CLs if cpm2_resource in cl.resources ), None)
-        
-        # x1=list(filter(lambda cl: (CL1 in cl), S.CL_NDs))[0][1]
-        # x2=list(filter(lambda cl: (CL2 in cl), S.CL_NDs))[0][1]
-        ND1=[l.ND_name for l in list(filter(lambda NT: (CL1 in NT.computationallayers), S.network_technologies))]
-        ND2=[l.ND_name for l in list(filter(lambda NT: (CL2 in NT.computationallayers), S.network_technologies))]
-        ND=list(set(ND1).intersection(ND2))
-      
-        if len(ND)==0:
-            print("ERROR: no network domain available between two resources "
-              + str(cpm1_resource)+" and "+str(cpm2_resource)) 
-            sys.exit(1)
-        
-        elif len(ND)==1:
-            l=list(filter(lambda network_technology: 
-                              (ND[0] in network_technology.ND_name), S.network_technologies))
-            # network_delay=l[0].performance_evaluator.evaluate(
-            #                    l[0].access_delay,l[0].bandwidth, S.data_sizes[comp_index][self.path.index(comp_index)+1])
-            comp2_key = [key for key, value in S.dic_map_com_idx.items() if value == self.path.index(comp_index)+1][0]
-           
-            network_delay=l[0].performance_evaluator.evaluate(
-                               l[0].access_delay,l[0].bandwidth,S.graph.G.get_edge_data(comp1_key, comp2_key)["data_size"])
-        
-        else :
-            comp2_key = [key for key, value in S.dic_map_com_idx.items() if value == self.path.index(comp_index)+1][0]
-            network_delay=float("inf")
-            for nd in ND:
-                l=list(filter(lambda network_technology: 
-                              (nd in network_technology.ND_name), S.network_technologies))
-                new_network_delay=l[0].performance_evaluator.evaluate(
-                               l[0].access_delay,l[0].bandwidth, S.graph.G.get_edge_data(comp1_key, comp2_key)["data_size"])
-                if new_network_delay<network_delay:
-                   network_delay=new_network_delay 
-        return network_delay
-     
-        
-    
+        self.path_name = path_name
+            
     
     ## Method to check the feasibility of the global constraints
     #   @param self The object pointer
-    #   @param S instance of System class
-    #   @param solution a solution of the problem
-    #   of the components in the path
-    #   return a boolean value
-    def check_feasibility(self,S,solution):
-        flag=False
-     
-        performance_of_components=[]
-        perf_evaluation=0
-        # check if the performance evaluation of components are not avalable, evaluate them
-       
+    #   @param S A System.System object
+    #   @param solution A Solution.Configuration representing a candidate 
+    #                   solution of the problem
+    #   @return A boolean which is true if the constraint is satisfied and 
+    #           the response time of the Path
+    def check_feasibility(self, S, solution):
+        
+        feasible = False
+        PE = SystemPerformanceEvaluator()
+        
+        # compute the response time of all components in the path
+        performance_of_components = []
+        perf_evaluation = 0
         for comp_index in self.path:
-            try:
-                perf_evaluation=self.get_perf_evaluation(S,solution.Y_hat,comp_index)
-            except:
-                pdb.set_trace()
-            
-            if not perf_evaluation==float("inf") and \
-                not np.isnan(perf_evaluation) and perf_evaluation>0:
-           
-                   performance_of_components.append(perf_evaluation)
+            perf_evaluation = PE.get_perf_evaluation(S, solution.Y_hat,
+                                                     comp_index)
+            # check if the response time is valid
+            if not perf_evaluation == float("inf") \
+                and not np.isnan(perf_evaluation) \
+                    and perf_evaluation > 0:
+                performance_of_components.append(perf_evaluation)
             else:
-                   solution.global_slack_value=float('Inf')
-                   return flag, float('Inf')
+                solution.global_slack_value=float('Inf')
+                return feasible, float('Inf')
                 
-        # sum the performance of all components in the current path
-        Sum=sum(performance_of_components)
+        # sum the response times of all components in the current path
+        Sum = sum(performance_of_components)
         
         # compute network delay
-        for comp_index in self.path:
-           #pdb.set_trace()
-           if self.path.index(comp_index)+1<len(self.path):
-               cpm1_resource=np.nonzero(solution.Y_hat[comp_index,:])[0][0]
-               cpm2_resource=np.nonzero(solution.Y_hat[self.path[self.path.index(comp_index)+1],:])[0][0]
-               if not cpm1_resource==cpm2_resource:
-                   network_delay=self.get_network_delay(comp_index,cpm1_resource,cpm2_resource,S)
-                   #if len(ND)>0:
-                       # l=list(filter(lambda network_technology: 
-                       #               (ND[0] in network_technology.ND_name), S.network_technologies))
-                           
-                       # network_delay=l[0].performance_evaluator.evaluate(
-                       #         l[0].access_delay,l[0].bandwidth, S.data_sizes[comp_index][self.path.index(comp_index)+1])
-                      
-                   Sum=Sum + network_delay
-                   # else:
-                   #     pdb.set_trace()
-                   #     ND=self.get_ND(cpm1_resource,cpm2_resource,S)
-                   #     print("ERROR: no network domain available between two resources "
-                   #           + str(cpm1_resource)+" and "+str(cpm2_resource))
-                           
-           
+        #
+        # loop over all components in the path
+        for idx in range(len(self.path) - 1):
+            
+            # get the indices of the current and the next component
+            comp_index = self.path[idx]
+            next_index = self.path[idx + 1]
+            
+            # get the indices of the resources where all partitions of the 
+            # current and the next component are executed
+            j = np.nonzero(solution.Y_hat[comp_index])
+            j1 = np.nonzero(solution.Y_hat[next_index])
+            
+            # resource index of the last partition
+            part1_resource = j[1][-1]
+            part2_resource = j1[1][0]
+            
+            # check if the two resources are different
+            if not part1_resource == part2_resource:
+                # get the names of the two components
+                comp1_key = S.components[comp_index].name
+                comp2_key = S.components[next_index].name
+                # get the amount of transferred data
+                data_size = S.graph.G.get_edge_data(comp1_key, comp2_key)["data_size"]
+                # compute the network delay
+                network_delay = PE.get_network_delay(part1_resource,
+                                                     part2_resource,
+                                                     S, data_size)
+                # update the total response time of the path                   
+                Sum += network_delay
+                
         # compute the slack value of the current solution
-        solution.global_slack_value=self.max_res_time - Sum
+        solution.global_slack_value = self.max_res_time - Sum
 
+        # check feasibility
         if Sum <= self.max_res_time:
-           flag=True 
+           feasible = True 
        
-        return flag, Sum
+        return feasible, Sum
+    
+    
+    ## Operator to convert a GlobalConstraint object into a string
+    #   @param self The object pointer
+    #   @param all_components List of all the Graph.Component objects in the 
+    #                         system
+    def __str__(self, all_components):
+        s = '"' + self.path_name + '": {"components": ['
+        for component_idx in self.path:
+            s += ('"' + all_components[component_idx].name + '",')
+        s = s[:-1] + '], "global_res_time": ' + str(self.max_res_time) + '}'
+        return s
+        
