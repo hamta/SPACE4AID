a3daedfcdac298c219be3d400c934662bfe9bf70
FFede0 <federica.filippini@polimi.it>
2021-11-22 17:04:33 +0100

    modified class and file names

---
+++ classes/PerformanceFactory.py
@@ -0,0 +1,48 @@
+from classes.PerformanceModels import FaaSPredictorPacsltk, FaaSPredictorMLlib
+from classes.PerformanceEvaluators import NetworkPerformanceEvaluator
+from classes.PerformanceEvaluators import ServerFarmPE, EdgePE
+
+
+## PerformanceFactory
+#
+# Class to build a factory of performance models/evaluators, characterized by 
+# a given key and used to predict the response time of 
+# Graph.Component.Partition objects executed on different Resources.Resource 
+class PerformanceFactory: 
+
+    ## @var models
+    # Dictionary of available predictors
+    
+    ## PerformanceFactory class constructor
+    def __init__(self):
+        self.models = {}
+
+    ## Method to register a new model/evaluator in the dictionary of available 
+    # predictors
+    #   @param self The object pointer
+    #   @param key The key used to identify the performance model
+    #   @param model The performance model
+    def register(self, key, model):
+        self.models[key] = model
+
+    ## Method to initialize a new model/evaluator from the factory
+    #   @param self The object pointer
+    #   @param key The key used to identify the performance model
+    #   @param **kwargs List of all parameter_name=parameter_value pairs 
+    #                   that are required to initialize the model
+    #   @return The performance model
+    def create(self, key, **kwargs):
+        model = self.models.get(key)
+        if not model:
+            raise ValueError(key)
+        return model(**kwargs)
+
+
+## Factory initialization
+PMfactory = PerformanceFactory()
+PMfactory.register("PACSLTK", FaaSPredictorPacsltk)
+PMfactory.register("MLLIBfaas", FaaSPredictorMLlib)
+PMfactory.register("QTcloud", ServerFarmPE)
+PMfactory.register("QTedge", EdgePE)
+PMfactory.register("NETWORK", NetworkPerformanceEvaluator)
+
