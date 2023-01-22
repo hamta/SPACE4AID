a5090b0163d7a2bbec7f2061df25e61bd9322b7a
System Administrator <hamta.sedghani@polimi.it>
2023-01-22 17:17:44 +0100

    Update with heuristics

--- Run_and_Evaluate_integrate_AISPRINT.py
+++ Run_and_Evaluate_integrate_AISPRINT.py
@@ -5,7 +5,7 @@ import system_file_json_generator
 import Input_json_generator
 from classes.Logger import Logger
 from classes.System import System
-from classes.Algorithm import Algorithm, RandomGreedy, Tabu_Search
+from classes.Algorithm import Algorithm, RandomGreedy, Tabu_Search, Simulated_Annealing, Genetic_algorithm
 import time
 import sys
 import os
@@ -148,49 +148,33 @@ def Random_Greedy_run(system_file,iteration_number_RG,seed,Max_time_RG,Lambda,K=
         elite_sol.append(elite.elite_results[i].solution)
     return elite_sol
 
-def TabuSearch_run(system_file,iteration_number_RG, max_iterations,
-                 seed,Max_time_RG, Max_time, Lambda, K=1, besties_RG=None):
+def TabuSearch_run(S,iteration_number_RG, max_iterations,
+                 seed,Max_time_RG, Max_time, method,tabu_memory,  K=1, besties_RG=None):
 
+            TS_Solid= Tabu_Search(iteration_number_RG,seed,system=S,Max_time_RG=Max_time_RG,K=K, besties_RG=besties_RG)
+            result, best_sol_info, Starting_points_info=TS_Solid.run_TS (method, tabu_memory, min_score=None, max_steps=max_iterations,Max_time=Max_time)
 
-              with open(system_file, "r") as a_file:
-                 json_object = json.load(a_file)
-              json_object["Lambda"] = Lambda
-              S = System(system_json=json_object)
-              method_list=["best"]# ,"random"
+            return result
 
-              for method in method_list:
+def SimulatedAnealing_run( S,iteration_number_RG, max_iterations,
+                         seed,Max_time_RG, Max_time,temp_begin,schedule_constant,K=1, besties_RG=None):
+                #temp_begin=5
+                #schedule_constant=0.99
 
-                  #  start=time.time()
-                   # proc = mpp.current_process()
-                   # pid = proc.pid
-                   # seed=seed*pid
+                SA_Solid= Simulated_Annealing(iteration_number_RG,seed,system=S,Max_time_RG=Max_time_RG,K=K, besties_RG=besties_RG)
+                result, best_sol_info, Starting_points_info=SA_Solid.run_SA( temp_begin, schedule_constant, max_iterations,  min_energy=None, schedule='exponential',Max_time=Max_time)
+                return result
 
-                   # GA=RandomGreedy(S,2)
-                    #random_greedy_result=GA.random_greedy( MaxIt=iteration_number_RG)
-                    # initial_solution=random_greedy_result[1].elite_results[0].solution
-                    # initial_cost=random_greedy_result[1].elite_results[0].solution.objective_function(S)
+def GeneticAlgorithm_run(S,iteration_number_RG, max_iteration_number,seed,
+                         K_init_population, Max_time_RG, Max_time, mutation_rate,crossover_rate, besties_RG=None):
 
-                    #
-                    #x=GA.change_component_placement(initial_solution)
-                    # y=GA.get_partitions_with_j(initial_solution,x[0])
+    #mutation_rate = 0.7
+    #crossover_rate = 0.5
+    GA = Genetic_algorithm(iteration_number_RG,seed, crossover_rate, mutation_rate, max_iteration_number, S,Max_time_RG=Max_time_RG,Max_time=Max_time, besties_RG=besties_RG)
 
+    result, best_sol_cost_list_GA, time_list_GA, population=GA.run_GA(K_init_population)
 
-
-                    tabu_memory=50
-                    #pdb.set_trace()
-                    start=time.time()
-
-                    TS_Solid= Tabu_Search(iteration_number_RG,seed,system=S,Max_time_RG=Max_time_RG,K=K, besties_RG=besties_RG)
-                    best_solution, best_cost,current_cost_list,best_cost_list, time_list=TS_Solid.run_TS (method, tabu_memory, min_score=None, max_steps=max_iterations,Max_time=Max_time)
-                    TS_time=time.time()-start
-                    #print()
-              result = Result()
-              result.solution=best_solution
-              result.objective_function(S)
-              feasible = result.check_feasibility(S)
-              return result
-
-
+    return result
 
 def main(input_dir,output_dir):
     error = Logger(stream = sys.stderr, verbose=1, error=True)
@@ -201,42 +185,105 @@ def main(input_dir,output_dir):
         Methods=input_json["Methods"]
         if "method1" in Methods.keys():
             name=Methods["method1"]["name"]
-            if name == "RandomGreedy":
+            if "random" in name.lower():
                 iteration_number_RG=Methods["method1"]["iterations"]
                 Max_time_RG=Methods["method1"]["duration"]
+            else:
+                error.log("{} should be random greedy.".format("method1"))
+                sys.exit(1)
         else:
-            error.log("{} does not exist".format("Methods"))
+            error.log("{} does not exist".format("Random greedy parameters"))
             sys.exit(1)
     else:
         error.log("{} does not exist".format("Methods"))
         sys.exit(1)
+    startingPointNumber = 1
     if "method2" in Methods.keys():
-        name=Methods["method2"]["name"]
-        startingPointNumber=Methods["method2"]["startingPointNumber"]
-        max_iteration_number=Methods["method2"]["iterations"]
-        Max_time=Methods["method2"]["duration"]
+        try:
+            method_name=Methods["method2"]["name"]
+            startingPointNumber=Methods["method2"]["startingPointNumber"]
+            max_iteration_number=Methods["method2"]["iterations"]
+            Max_time=Methods["method2"]["duration"]
+        except Exception as e:
+           error.log(" parameter {} does not exist".format(e))
+           sys.exit(1)
+
+    if "Seed" in input_json.keys():
+        seed=input_json["Seed"]
+    else:
+        error.log("{} does not exist".format("Seed"))
+        sys.exit(1)
+    #seed=1
     print("\nStart parsing YAML files... ")
     system_file=system_file_json_generator.make_system_file(input_dir)
     #system_file = input_dir+"/SystemFile-Demo.json"#"ConfigFiles/RG-MaskDetection.json" # "ConfigFiles/Random_Greedy.json"
 
-
-
-    start_lambda = 0.0000014
-    end_lambda = 0.0000015
-    step = 0.0000001
-    seed = 2
-    Lambda = start_lambda
     system_file = create_pure_json(system_file)
     with open(system_file, "r") as a_file:
         json_object = json.load(a_file)
-    json_object["Lambda"] = Lambda
+    #json_object["Lambda"] = Lambda
 
     print("\n Start parsing config files... ")
     S = System(system_json=json_object)#, log=Logger(verbose=2))
     print("\n Start searching by  Random Greedy ... ")
-    starting_points=Random_Greedy_run(system_file,iteration_number_RG,seed,Max_time_RG,Lambda,startingPointNumber)
-    print("\n Start searching by  Local search ... ")
-    result=TabuSearch_run(system_file,iteration_number_RG, max_iteration_number, seed,Max_time_RG, Max_time,Lambda, besties_RG=starting_points)
+    starting_points=Random_Greedy_run(system_file,iteration_number_RG,seed,Max_time_RG,S.Lambda,startingPointNumber)
+    if len(starting_points)<1:
+        error.log("No feasible solution is found by RG")
+        sys.exit(1)
+    if "method2" in Methods.keys():
+        print("\n Start searching by heuristic method ... ")
+        if "tabu" in method_name.lower():
+            if "specialParameters" in Methods["method2"].keys():
+                if "tabuSize" in Methods["method2"]["specialParameters"].keys():
+                    tabu_size= Methods["method2"]["specialParameters"]["tabuSize"]
+                else:
+                    error.log("{} does not exist".format("Tabue memory"))
+                    sys.exit(1)
+            else:
+                error.log("{} does not exist".format("specialParameters"))
+                sys.exit(1)
+
+            result=TabuSearch_run(S,iteration_number_RG, max_iteration_number, seed,Max_time_RG, Max_time,"random",tabu_size,K=1, besties_RG=starting_points)
+        elif "local" in method_name.lower():
+            result=TabuSearch_run(S,iteration_number_RG, max_iteration_number, seed,Max_time_RG, Max_time,"best",1, K=1, besties_RG=starting_points)
+        elif "simulated" in method_name.lower():
+            if "specialParameters" in Methods["method2"].keys():
+                if "temperature" in Methods["method2"]["specialParameters"].keys():
+                    temp_begin= Methods["method2"]["specialParameters"]["temperature"]
+                else:
+                    error.log("{} does not exist".format("initial temperature"))
+                    sys.exit(1)
+                if "scheduleConstant" in Methods["method2"]["specialParameters"].keys():
+                    schedule_constant= Methods["method2"]["specialParameters"]["scheduleConstant"]
+                    result = SimulatedAnealing_run(S,iteration_number_RG, max_iteration_number, seed,Max_time_RG, Max_time,temp_begin,schedule_constant,K=1, besties_RG=starting_points)
+                else:
+                    error.log("{} does not exist".format("temperature"))
+                    sys.exit(1)
+            else:
+                error.log("{} does not exist".format("specialParameters"))
+                sys.exit(1)
+        elif "genetic" in method_name.lower():
+            if "specialParameters" in Methods["method2"].keys():
+                if "mutationRate" in Methods["method2"]["specialParameters"].keys():
+                    mutation_rate= Methods["method2"]["specialParameters"]["mutationRate"]
+                else:
+                    error.log("{} does not exist".format("mutation rate"))
+                    sys.exit(1)
+                if "crossoverRate" in Methods["method2"]["specialParameters"].keys():
+                    crossover_rate = Methods["method2"]["specialParameters"]["crossoverRate"]
+                    result = GeneticAlgorithm_run(S,iteration_number_RG, max_iteration_number, seed, startingPointNumber,Max_time_RG, Max_time,mutation_rate,crossover_rate, besties_RG=starting_points)
+                else:
+                    error.log("{} does not exist".format("crossover rate"))
+                    sys.exit(1)
+
+            else:
+                error.log("{} does not exist".format("specialParameters"))
+                sys.exit(1)
+    else:
+        result = Result()
+        result.solution = starting_points[0]
+        result.objective_function(S)
+
 
     output_json=output_dir+"/Output.json"
     result.print_result(S,output_json)
