9b02a23101360b5c5256cbd72b836fe5a0529cbc
Hamta Sedghani;Hamta Sedghani <hamta.sedghani@polimi.it>
2023-02-23 08:59:22 +0100

    Revert "Update xgboost_experiment_configuration.py"
    This reverts commit 7bd8329db335d6e11c1b5a0c492b6ee56cabc04b

--- a-MLlibrary/model_building/xgboost_experiment_configuration.py
+++ a-MLlibrary/model_building/xgboost_experiment_configuration.py
@@ -2,11 +2,13 @@
 Copyright 2019 Marco Lattuada
 Copyright 2019 Danilo Ardagna
 Copyright 2021 Bruno Guindani
-Copyright 2022 Nahuel Coliva
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
      http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,22 +28,30 @@ import model_building.experiment_configuration as ec
 class XGBoostExperimentConfiguration(ec.ExperimentConfiguration):
     """
     Class representing a single experiment configuration for XGBoost
+
     Methods
     -------
     _compute_signature()
         Compute the signature (i.e., an univocal identifier) of this experiment
+
     _train()
         Performs the actual building of the linear model
+
     compute_estimations()
         Compute the predictions for data points indicated in rows estimated by the regressor
+
     print_model()
         Print the representation of the generated model
+
     initialize_regressor()
         Initialize the regressor object for the experiments
+
     get_default_parameters()
         Get a dictionary with all technique parameters with default values
+
     repair_hyperparameters()
         Repair and return hyperparameter values which cause the regressor to raise errors
+
     get_weights_dict()
         Return a dictionary containing the regressor's normalized importance weights for each feature
     """
@@ -49,8 +59,10 @@ class XGBoostExperimentConfiguration(ec.ExperimentConfiguration):
         """
         campaign_configuration: dict of dict:
             The set of options specified by the user though command line and campaign configuration files
+
         hyperparameters: dictionary
             The set of hyperparameters of this experiment configuration
+
         regression_inputs: RegressionInputs
             The input of the regression problem to be solved
         """
@@ -60,10 +72,12 @@ class XGBoostExperimentConfiguration(ec.ExperimentConfiguration):
     def _compute_signature(self, prefix):
         """
         Compute the signature associated with this experiment configuration
+
         Parameters
         ----------
         prefix: list of str
             The signature of this experiment configuration without considering hyperparameters
+
         Returns
         -------
             The signature of the experiment
@@ -88,17 +102,18 @@ class XGBoostExperimentConfiguration(ec.ExperimentConfiguration):
             warnings.simplefilter("ignore")
             self._regressor.fit(xdata, ydata)
         self._logger.debug("---Model built")
-        weights = self.get_weights_dict(self._regressor)
-        for key, val in weights.items():
+        for key, val in self.get_weights_dict().items():
             self._logger.debug("The weight for %s is %f", key, val)
 
     def compute_estimations(self, rows):
         """
         Compute the predictions for data points indicated in rows estimated by the regressor
+
         Parameters
         ----------
         rows: list of integers
             The set of rows to be considered
+
         Returns
         -------
             The values predicted by the associated regressor
@@ -112,8 +127,7 @@ class XGBoostExperimentConfiguration(ec.ExperimentConfiguration):
         """
         Print the representation of the generated model
         """
-        weights = self.get_weights_dict(self._regressor)
-        sorted_weights = dict(sorted(weights.items(), key=operator.itemgetter(1), reverse=True))
+        sorted_weights = dict(sorted(self.get_weights_dict().items(), key=operator.itemgetter(1), reverse=True))
         ret = "XGBoost weights: {\n"
         for key, val in sorted_weights.items():
             ret += "    " + str(round(val, 3)) + " " + key + "\n"
@@ -123,17 +137,11 @@ class XGBoostExperimentConfiguration(ec.ExperimentConfiguration):
     def initialize_regressor(self):
         """
         Initialize the regressor object for the experiments
-        See ExperimentConfiguration class for _disable_model_parallelism
         """
         if not getattr(self, '_hyperparameters', None):
             self._regressor = xgb.XGBRegressor()
         else:
-            if self._disable_model_parallelism:
-                self._regressor = xgb.XGBRegressor(min_child_weight=self._hyperparameters['min_child_weight'], gamma=self._hyperparameters['gamma'], n_estimators=self._hyperparameters['n_estimators'], learning_rate=self._hyperparameters['learning_rate'], max_depth=self._hyperparameters['max_depth'], tree_method="hist", objective='reg:squarederror', nthread=1, n_jobs=1)
-                self._logger.debug("\nSetup model with no parallelism\n")
-            else:
-                self._regressor = xgb.XGBRegressor(min_child_weight=self._hyperparameters['min_child_weight'], gamma=self._hyperparameters['gamma'], n_estimators=self._hyperparameters['n_estimators'], learning_rate=self._hyperparameters['learning_rate'], max_depth=self._hyperparameters['max_depth'], tree_method="hist", objective='reg:squarederror', n_jobs=1)
-                self._logger.debug("\nSetup model with parallelism\n")
+            self._regressor = xgb.XGBRegressor(min_child_weight=self._hyperparameters['min_child_weight'], gamma=self._hyperparameters['gamma'], n_estimators=self._hyperparameters['n_estimators'], learning_rate=self._hyperparameters['learning_rate'], max_depth=self._hyperparameters['max_depth'], tree_method="hist", objective='reg:squarederror', n_jobs=1)
 
     def get_default_parameters(self):
         """
@@ -148,10 +156,12 @@ class XGBoostExperimentConfiguration(ec.ExperimentConfiguration):
     def repair_hyperparameters(self, hypers):
         """
         Repair and return hyperparameter values which cause the regressor to raise errors
+
         Parameters
         ----------
         hypers: dict of str: object
             the hyperparameters to be repaired
+
         Return
         ------
         dict of str: object
@@ -162,12 +172,11 @@ class XGBoostExperimentConfiguration(ec.ExperimentConfiguration):
             new_hypers[key] = int(new_hypers[key])
         return new_hypers
 
-    @staticmethod
-    def get_weights_dict(xgb_regressor):
+    def get_weights_dict(self):
         """
         Return a dictionary containing the regressor's normalized importance weights for each feature
         """
-        weights = xgb_regressor.get_booster().get_fscore()
+        weights = self._regressor.get_booster().get_fscore()
         weights_sum = sum(weights.values())
         for key in weights:
             weights[key] /= weights_sum
